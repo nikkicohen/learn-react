e3398c15b09d88e2022eeaeae901c5c886f50008
nikkicohen <cohen.nik@northeastern.edu>
2024-02-09 12:03:10 -0500

    in class activities 0208

--- src/pages/qcomps/firstcomp.js
+++ src/pages/qcomps/firstcomp.js
@@ -1,1 +1,12 @@
-function MyComp() {}
+export default function FirstComp() {
+    return (
+        <div>
+            <h1>My first component</h1>
+            <ol>
+                <li>Components: UI Building Block</li>
+                <li>Defining a Component</li>
+                <li>Using a Component</li>
+            </ol>
+        </div>
+    )
+}
