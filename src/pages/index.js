dcfbe6a237be7229aecc973e26ca68aca6d37c6d
nikkicohen <cohen.nik@northeastern.edu>
2024-02-15 16:32:36 -0500

    in class activities 2/15

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,19 @@
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Profile from './components/profile'
+import List from './qcomps/list_keys_id'
+import Recipe from './qcomps/recipes'
+import Gallery from './qcomps/state'
+import PersonData from './components/stateObj'
+import Form from './qcomps/stuckForm'
+import Thing from './components/snapshot2'
+import Queue from './components/queueUpdates'
+import Shop from './qcomps/shoppingCart'
 
 export default function Home() {
   return (
     <div className={styles.main}>
-        <Profile />
+        <Shop />
     </div>
   )
 }
--- src/pages/qcomps/recipes.js
+++ src/pages/qcomps/recipes.js
@@ -12,11 +12,23 @@ export const recipes = [{
   ingredients: new Set(['chickpeas', 'olive oil', 'garlic cloves', 'lemon', 'tahini'])
 }];
 
+
+
 export default function RecipeList() {
+  const recipesItems = recipes.map((recipe) => 
+    <div key = {recipe.id}>
+      <h2>{recipe.name}</h2>
+      <ul>
+        {Array.from(recipe.ingredients).map((ingredient) => 
+          <li key = {ingredient}>{ingredient}</li>
+        )}
+      </ul>
+    </div>
+  );
   return (
     <div>
       <h1>Recipes</h1>
-      {}
+      <ul>{recipesItems}</ul>
     </div>
   );
 }
